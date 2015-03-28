05af16dff2937330a29d59d1a4bfc8b74798e22c
Terry <tchan247@gmail.com>
2015-03-27 22:19:33 -0700

    Made stringifyJSON function and passed its test cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(Array.isArray(obj)) {  // handle arrays
+
+    var elements = [];
+    for(var i=0, len = obj.length; i<len; i++) {
+      elements.push(stringifyJSON(obj[i]));  //  recursive call to handle each element
+    }
+
+    return '[' + elements + ']';
+
+  } else if (typeof obj === 'object' && obj !== null) {  // handle object literals
+
+    var pair = [];
+    for(var i in obj) {
+      if(stringifyJSON(obj[i]) !== '') {
+        pair.push(stringifyJSON(i) + ':' + stringifyJSON(obj[i]));
+      }
+    }
+
+    return '{' + pair + '}';
+
+  } else if (typeof obj === 'function') { // handle functions
+
+    return stringifyJSON(obj());
+
+  } else if (typeof obj === 'string') {  //handle strings
+
+    return '"' + obj + '"';
+
+  } else if (obj === undefined) {  // handle undefined
+
+    return '';
+
+  } else {
+
+    return '' + obj;
+
+  }
+
 };
