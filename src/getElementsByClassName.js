1d8c71d806a07aa61424d38497495b601199ec92
Terry <tchan247@gmail.com>
2015-03-28 11:54:52 -0700

    Implemented getElementsByClassName with extra parameter for recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, node){  // added node parameter for recursion
+
+  node = node || document.body;  // assign current node
+
+  var children = node.childNodes;
+  var arr = [];
+
+  // push current node if it has class className
+  if(node.classList && node.classList.contains(className)) {
+    arr.push(node);
+  }
+
+  // check for className in each child node
+  for(var i=0, len=children.length; i<len; i++) {
+
+    if(children[i].childNodes.length > 0) {
+      arr = arr.concat(getElementsByClassName(className, children[i]));  // recursive call to check rest of tree
+    }
+
+    if(children[i].classList && children[i].classList.contains(className) && arr.indexOf(children[i]) === -1) {
+      arr.push(children[i]);
+    }
+
+  }
+
+  return arr;
 };
